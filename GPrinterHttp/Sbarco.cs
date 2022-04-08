25eb017a67093a0be1461fc692e860b823b13feb
JamesLiao <vivlonglz@gmail.com>
2022-04-08 14:27:19 +0800

    update

--- GPrinterHttp/Sbarco.cs
+++ GPrinterHttp/Sbarco.cs
@@ -4,13 +4,31 @@ namespace GPrinterHttp
 {
 				public static class Sbarco
 				{
+								// Port Functions
 								[DllImport("SBSDK_API.dll", CallingConvention = CallingConvention.StdCall)]
 								public static extern bool PortOpen(string pPortName);
 
-								[DllImport("SBSDK_API.dll", CallingConvention = CallingConvention.StdCall)]
+								[DllImport("SBSDK_API.dll")]
+								public static extern void PortClose();
+
+								[DllImport("SBSDK_API.dll")]
 								public static extern int PortEnumCount(int nPortType);
 
-								[DllImport("SBSDK_API.dll", CallingConvention = CallingConvention.StdCall)]
+								[DllImport("SBSDK_API.dll")]
 								public static extern bool PortEnumGet(int nPortType, int nItem, string pBuffer);
+
+								// Setting Functions
+								[DllImport("SBSDK_API.dll")]
+								public static extern bool SetMeasurement(int nMeasurement, int nResolution);
+
+								// Print Functions
+								[DllImport("SBSDK_API.dll")]
+								public static extern bool PrintText(float fX, float fY, int nRotation, int nFontID, int nFontHor, int nFontVer, bool bReverse, int nDataLen, string pData);
+								
+								[DllImport("SBSDK_API.dll")]
+								public static extern bool PrintBarcode(float fX, float fY, float fBarHeight, int nRotation, int nBarType, int nNarrowBar, int nWideBar, bool bHuman, int nDataLen, string pData);
+
+								[DllImport("SBSDK_API.dll")]
+								public static extern bool PrintLabel(int nSet, int nCopy, bool bReverse);
 				}
 }
